9341cfabbc1afb1f394346fee9137335e086b50c
funZbunZ45678 <madelinemarie.tx@gmail.com>
2021-04-27 14:09:39 -0500

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -16,20 +16,20 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
     	//TEST
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Roomiez");
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
  
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println("HelWorld!");
             }
         });
         
         StackPane root = new StackPane();
         root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.setScene(new Scene(root, 500, 600));
         primaryStage.show();
     }
 }
