f95a30f407fc3b808cbb09d5654493bf0bf8feea
funZbunZ45678 <madelinemarie.tx@gmail.com>
2021-04-30 11:26:54 -0500

    Fixed Madelines Machine

--- src/application/Main.java
+++ src/application/Main.java
@@ -14,17 +14,14 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
     	try {
-			// Define the basic calculator (default) scene
 			Parent mainParent = FXMLLoader.load(getClass().getResource("/application/views/Main.fxml"));
 			Scene mainScene = new Scene(mainParent,400,400);
 			mainScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			
-			// Define the date calculator scene
 			Parent homeParent = FXMLLoader.load(getClass().getResource("/application/views/HomeScreen.fxml"));
 			Scene homeScene = new Scene(homeParent,400,400);
 			homeScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());			
 			
-			// Define the temperature calculator scene
 			Parent moveInChecklistParent = FXMLLoader.load(getClass().getResource("/application/views/MoveInChecklist.fxml"));
 			Scene moveInChecklistScene = new Scene(moveInChecklistParent,400,400);
 			moveInChecklistScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
@@ -35,7 +32,6 @@ public class Main extends Application {
 			SceneSwitcher.AddScene(homeScene, "home");
 			SceneSwitcher.AddScene(moveInChecklistScene, "move");
 			
-			// Set the default scene (basic calculator)
 			primaryStage.setScene(mainScene);
 			primaryStage.show();
 		
