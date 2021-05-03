4da7fdb441c5384cd2c4e0aba1cc6b1641c57eb0
funZbunZ45678 <madelinemarie.tx@gmail.com>
2021-05-03 12:14:42 -0500

    Added Back Buttons

--- src/application/controllers/GroceryListController.java
+++ src/application/controllers/GroceryListController.java
@@ -3,10 +3,10 @@ package application.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
-public class MoveInChecklistController extends Controller {
+public class GroceryListController extends Controller{
 	
 	@FXML
-	public void handleBackMove(ActionEvent e) {
+	public void handleBackGrocery(ActionEvent e) {
 		viewChanged("home");
 	}
 
