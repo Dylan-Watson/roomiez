f54ab37cc397de84f825776a282ccb0ef63f1e5a
Dylan Watson <watson.dylan@centaurisoftware.co>
2021-05-03 12:50:26 -0500

    notes about the move in list.

--- src/application/controllers/MoveInChecklistController.java
+++ src/application/controllers/MoveInChecklistController.java
@@ -10,4 +10,18 @@ public class MoveInChecklistController extends Controller {
 		viewChanged("home");
 	}
 
+	@FXML
+	public void handleAddBtnClicked(ActionEvent e) {
+		// Change the view to the add form thingy
+			// save button, assignments(?), name
+		// On save button press, send info to model to get saved to txt file
+			// Model will use code + grocerylist to save the txt file, that way it can again be loaded in later after login
+			// Login code needs to be stored in an accessible class (singleton?) in memory
+				// Likely the login should act like a singleton but there is no need to write the logic to make it one
+	}
+	
+	@FXML
+	public void handleSubBtnClicked(ActionEvent e) {
+		// How do I want this to work? The existance of this button means that the checklist items need to be selectable
+	}
 }
