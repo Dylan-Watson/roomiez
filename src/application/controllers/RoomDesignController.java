2047396a361931025770d589c5c89d99340a35e7
funZbunZ45678 <madelinemarie.tx@gmail.com>
2021-05-04 12:24:38 -0500

    Update RoomDesignController.java

--- src/application/controllers/RoomDesignController.java
+++ src/application/controllers/RoomDesignController.java
@@ -1,6 +1,5 @@
 package application.controllers;
 
-//import application.SceneSwitcher;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.event.ActionEvent;
@@ -11,12 +10,12 @@ import javafx.scene.paint.Color;
 public class RoomDesignController extends Controller
 {
 	@FXML 
-	private Canvas canvas;
+	public Canvas cc;
 	
-	/*public void initialize ()
-	{
-    	//GraphicsContext gc = canvas.getGraphicsContext2D();
-	}*/
+	//public void initialize ()
+	//{
+    	private GraphicsContext gc = cc.getGraphicsContext2D();
+	//}
     
 	
 	
@@ -29,15 +28,18 @@ public class RoomDesignController extends Controller
 	@FXML
 	public void handleRectangle(ActionEvent e)
 	{
-		GraphicsContext gc = canvas.getGraphicsContext2D();
+		System.out.println("Hey!");
+		
+	}
+	
+	public void drawRectangle(GraphicsContext gc)
+	{
 		gc.setFill(Color.GREEN);
         gc.setStroke(Color.BLUE);
         gc.setLineWidth(2.0);
         gc.strokeLine(40, 10, 10, 40);
         gc.fillOval(10, 60, 30, 30);
         gc.strokeOval(60, 60, 30, 30);
-        gc.fillRoundRect(110, 60, 30, 30, 10, 10);
-        gc.strokeRoundRect(160, 60, 30, 30, 10, 10);
 	}
 }
 
